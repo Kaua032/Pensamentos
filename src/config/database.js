0c727950e6c544debdd17aa55aabdf6911a8ae87
Kauã Xavier <kauaxavier032@gmail.com>
2023-10-02 11:50:16 -0300

    feat: database config created

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+export default {
+    dialect: "mysql",
+    host: "localhost",
+    username: "aluno_medio",
+    password: "@lunoSenai23.",
+    database: "db_pensamentos",
+    define: {
+        timestamps: true,
+        underscored: true,
+    }
+};
