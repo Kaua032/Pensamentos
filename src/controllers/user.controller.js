3e1741510f0275efc30b780825ec051e90bb08ab
Kauã Xavier <kauaxavier032@gmail.com>
2023-10-04 07:52:39 -0300

    feat: Added login route and controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,11 +5,9 @@ const CreateUserController = async (req, res) => {
 
   try {
     if (!name || !email || !password) {
-      res
-        .status(400)
-        .send({
-          message: "Preencha todos os campos para cadastrar o seu perfil.",
-        });
+      res.status(400).send({
+        message: "Preencha todos os campos para cadastrar o seu perfil.",
+      });
     }
     const user = await User.create({ name, email, password });
 
@@ -19,23 +17,49 @@ const CreateUserController = async (req, res) => {
   }
 };
 
-const UpdateUserController = async (req, res) => {
-  const { name, email, password } = req.body;
-  const { id } = req.params;
-  
-  try {
-    if(!name && !email && !password){
-      res
-        .status(400)
-        .send({
-          message: "Preencha pelo menos um dos campos para atualizar o seu perfil",
-        });
-    }
-    const userUpdated = User.update({id: id, name, email, password })
+const LoginController = async (req, res) => {
+  const { email, password } = req.body;
 
+  try {
+    if (!email || !password) {
+      res.status(400).send({
+        message: "Preencha todos os campos para cadastrar o seu perfil.",
+      });
+    }
+
+    const user = await User.findOne({ email: email, password: password });
+
+    res.status(200).send(user);
   } catch (error) {
     res.status(500).send({ message: error.message });
   }
-}
+};
 
-module.exports = { CreateUserController,  UpdateUserController};
+const UpdateUserController = async (req, res) => {
+  const { name, email, password } = req.body;
+  const { id } = req.params;
+
+  try {
+    if (!name && !email && !password) {
+      res.status(400).send({
+        message:
+          "Preencha pelo menos um dos campos para atualizar o seu perfil",
+      });
+    }
+    const userUpdated = await User.update(
+      { id: id },
+      { name: name, email: email, password: password }
+    );
+    console.log(userUpdated);
+
+    res.status(201).send(userUpdated);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+};
+
+module.exports = {
+  CreateUserController,
+  LoginController,
+  UpdateUserController,
+};
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,8 +1,13 @@
 const { Router } = require("express");
-const { CreateUserController, UpdateUserController } = require("../controllers/user.controller.js");
+const {
+  CreateUserController,
+  UpdateUserController,
+  LoginController,
+} = require("../controllers/user.controller.js");
 const userRouter = Router();
 
 userRouter.post("/create", CreateUserController);
-userRouter.patch("/update/:id?", UpdateUserController)
+userRouter.get("/login", LoginController);
+userRouter.patch("/update/:id", UpdateUserController);
 
 module.exports = userRouter;
