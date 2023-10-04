62d0a48669a7cad85c01441d4e7625bb41e3617a
Kauã Xavier <kauaxavier032@gmail.com>
2023-10-03 23:40:41 -0300

    feat: The diff adds User model to the database.

--- src/database/index.js
+++ src/database/index.js
@@ -1,10 +1,12 @@
 const Sequelize = require('sequelize');
 const db = require('../config/database.js');
 const Thought = require('../models/Thought.js');
+const User = require('../models/User.js');
 
 const connection = new Sequelize(db);
 
 Thought.init(connection);
+User.init(connection);
 console.log('Database Connected');
 
 module.exports = connection;
