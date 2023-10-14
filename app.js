40f05b58d4b3cff24a81095ba60015166de353ed
Kauã Xavier <kauaxavier032@gmail.com>
2023-10-14 06:07:57 -0300

    feat: Update path and express dependency to use posix compliant path separators

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 const express = require("express");
 const router = require("./src/routes/index.js");
-const exphbs = require('express-handlebars').create();
-const path = require('path')
+const exphbs = require("express-handlebars").create();
+const path = require("path");
 
 require("./src/database/index.js");
 
 const app = express();
 
-app.set('views', path.join(__dirname, 'src/views'));
-app.use(express.static('src/public'));
-app.use(express.urlencoded({extended: true}));
+app.set("views", path.join(__dirname, "src/views"));
+app.use(express.static("src/public"));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.engine('handlebars', exphbs.engine);
-app.set('view engine', 'handlebars');
+app.engine("handlebars", exphbs.engine);
+app.set("view engine", "handlebars");
 
 app.use(router);
 
